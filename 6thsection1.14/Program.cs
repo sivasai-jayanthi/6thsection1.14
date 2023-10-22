1baf80ff25e85ef957c557c9f21722422075e468
sivasai-jayanthi <sai89938@gmail.com>
2023-10-22 17:06:50 +0530

    Add project files.

---
+++ 6thsection1.14/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+
+namespace _6thsection1._14
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+
+			readtext();
+
+			Console.ReadLine();
+		}
+
+
+
+
+		public static void readtext()
+		{
+			FileStream fs = new FileStream(@"C:\Users\sai\source\repos\6thsection1.14\student data.txt", FileMode.Open, FileAccess.Read);
+			StreamReader sr = new StreamReader(fs);
+			var data = sr.ReadToEnd();
+			Console.WriteLine(data);
+			sr.Close();
+			sr.Dispose();
+
+
+		}
+
+
+
+
+	}
+}
